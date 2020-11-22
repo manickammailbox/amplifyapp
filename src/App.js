2caf832890d9b228c8763c1fa0e27fea4e197f23
manickammailbox <manickam.mailbox@gmail.com>
2020-11-22 19:45:13 +0530

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+/*
 function App() {
   return (
     <div className="App">
@@ -23,3 +39,5 @@ function App() {
 }
 
 export default App;
+		*/
+	
